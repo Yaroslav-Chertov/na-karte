0b7a4ad224a55f65a220087abbe3ad37a8d771da
Yaroslav Chertov <yachertov@yandex.ru>
2023-10-08 18:38:20 +0300

    реализовать попап

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,36 @@
-const tooltip = document.querySelector('.tooltip');
-const areas = document.querySelectorAll('.area');
+let continents = document.querySelectorAll('.area');
+let popupBg = document.querySelector('.popup__bg');
+let popup__photo = document.querySelector('.popup__photo');
+let popup__title = document.querySelector('.popup__title');
+let popup__text = document.querySelector('.popup__text');
+let tooltip = document.querySelector('.tooltip');
 
-areas.forEach(area => {
-    area.addEventListener('mousemove', function (e) {
-        tooltip.innerText = this.dataset.title;
-        tooltip.style.top = (e.y + 20) + 'px';
-        tooltip.style.left = (e.x + 20) + 'px';
+continents.forEach((item) => {
+    item.addEventListener('click', function () {
+        popup__title.textContent = this.getAttribute('data-title');
+        popup__photo.setAttribute('src', this.getAttribute('data-photo'));
+        popup__text.textContent = this.getAttribute('data-description');
+        popupBg.classList.add('active');
     });
 
-    area.addEventListener('mouseenter', function () {
+    item.addEventListener('mouseenter', function () {
+        tooltip.textContent = item.getAttribute('data-title');
         tooltip.style.display = 'block';
     });
 
-    area.addEventListener('mouseleave', function () {
+    item.addEventListener('mouseleave', function () {
+        tooltip.textContent = item.getAttribute('data-title');
         tooltip.style.display = 'none';
     });
+
+    item.addEventListener('mousemove', function (e) {
+        tooltip.style.top = (e.y + 20) + 'px';
+        tooltip.style.left = (e.x + 20) + 'px';
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+        popupBg.classList.remove('active');
+    }
 });
