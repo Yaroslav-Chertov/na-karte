4bf1307e5a07e1b492b83f25a59405145885a9b5
Yaroslav Chertov <yachertov@yandex.ru>
2023-11-28 21:23:35 +0300

    убрать тултип

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-let continents = document.querySelectorAll('.area');
-let popupBg = document.querySelector('.popup__bg');
-let popup__photo = document.querySelector('.popup__photo');
-let popup__title = document.querySelector('.popup__title');
-let popup__text = document.querySelector('.popup__text');
-let tooltip = document.querySelector('.tooltip');
+const continents = document.querySelectorAll('.area');
+const popupBg = document.querySelector('.popup__bg');
+const popup__photo = document.querySelector('.popup__photo');
+const popup__title = document.querySelector('.popup__title');
+const popup__text = document.querySelector('.popup__text');
 
 continents.forEach((item) => {
     item.addEventListener('click', function () {
@@ -11,21 +10,6 @@ continents.forEach((item) => {
         popup__photo.setAttribute('src', this.getAttribute('data-photo'));
         popup__text.textContent = this.getAttribute('data-description');
         popupBg.classList.add('active');
-    });
-
-    item.addEventListener('mouseenter', function () {
-        tooltip.textContent = item.getAttribute('data-title');
-        tooltip.style.display = 'block';
-    });
-
-    item.addEventListener('mouseleave', function () {
-        tooltip.textContent = item.getAttribute('data-title');
-        tooltip.style.display = 'none';
-    });
-
-    item.addEventListener('mousemove', function (e) {
-        tooltip.style.top = (e.y + 20) + 'px';
-        tooltip.style.left = (e.x + 20) + 'px';
     });
 });
 
